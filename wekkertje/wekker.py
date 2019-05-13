6c09c6ab73a5eee526be1cd5bc7f3e264a4eafd5
Fons van der Plas <fonsvdplas@gmail.com>
2019-05-13 23:43:06 +0200

    looop

--- wekkertje/wekker.py
+++ wekkertje/wekker.py
@@ -6,19 +6,20 @@ from morse import play_text, lampuit
 
 def refresh_git(also_do_hard_reset=True):
     if also_do_hard_reset:
-        subprocess.call("git reset --hard", shell=True, cwd="..")
-    subprocess.call("git pull", shell=True, cwd="..")
+        subprocess.call("git reset --hard", shell=True, stdout=subprocess.PIPE, cwd="..")
+    subprocess.call("git pull", shell=True, stdout=subprocess.PIPE, cwd="..")
 
 
 if __name__ == "__main__":
-    refresh_git()
-    if check():
-        lampuit()
-        # 1 minuutje wachten tot de volgende pull
-        # te vaak pullen wordt misschien geblokkeerd
-        time.sleep(60)
-    else:
-        with open("geheimwoordje.txt") as f:
-            geheimwoordje_text = f.readline()
-        play_text(geheimwoordje_text)
-        lampuit()
+    while True:
+        refresh_git()
+        if check():
+            lampuit()
+            # 1 minuutje wachten tot de volgende pull
+            # te vaak pullen wordt misschien geblokkeerd
+            time.sleep(60)
+        else:
+            with open("geheimwoordje.txt") as f:
+                geheimwoordje_text = f.readline()
+            play_text(geheimwoordje_text)
+            lampuit()
